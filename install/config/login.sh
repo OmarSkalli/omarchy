425524ca245ca97301b40a0ac452731f03d8589a
Omar Skalli <omar.skalli7@gmail.com>
2025-08-11 17:19:05 -0700

    Lock uwsm version to 0.23.0

--- install/config/login.sh
+++ install/config/login.sh
@@ -2,7 +2,7 @@
 
 # Hyprland launched via UWSM and login directly as user, rely on disk encryption + hyprlock for security
 if ! command -v uwsm &>/dev/null || ! command -v plymouth &>/dev/null; then
-  yay -S --noconfirm --needed uwsm plymouth
+  yay -S --noconfirm --needed uwsm=0.23.0 plymouth
 fi
 
 # ==============================================================================
